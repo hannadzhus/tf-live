e4051286ea40a2ccd682a516b962deb43fc77041
“Hanna <anna.dzhus@gmail.com>
2024-03-15 20:05:43 -0500

    modified

--- main.tf
+++ main.tf
@@ -1,11 +1,26 @@
 provider "aws" {
     region = "us-east-2"  
 }
+
+terraform {
+  required_version = "0.13.0"
+  required_providers {
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = "~> 0.1"
+    }
+    mycloud = {
+      source  = "hashicorp/aws"
+      version = ">= 1.0"
+    }
+  }
+}
+
+
 resource "awscc_ecr_repository" "second-repo" {
   repository_name      = "any-app2"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration = {
     scan_on_push = true
   }
-
 }
